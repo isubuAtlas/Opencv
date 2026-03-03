281ddb1c79c71f45b88b82afaa5d289864a59d7c
elifaytac05 <elifaytac005@gmail.com>
2026-03-03 16:16:11 +0300

    Update sayim.py

--- elif/sayim.py
+++ elif/sayim.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from collections import Counter
 
@@ -17,23 +16,3 @@ for file in os.listdir(labels_path):
 print("Class dağılımı:")
 for k, v in sorted(counter.items()):
     print(f"Class {k}: {v} adet")
-=======
-import os
-from collections import Counter
-
-labels_path = "final_dataset/train/labels"
-  # eğer hata verirse yolu değiştiririz
-
-counter = Counter()
-
-for file in os.listdir(labels_path):
-    if file.endswith(".txt"):
-        with open(os.path.join(labels_path, file), "r") as f:
-            for line in f:
-                class_id = line.strip().split()[0]
-                counter[class_id] += 1
-
-print("Class dağılımı:")
-for k, v in sorted(counter.items()):
-    print(f"Class {k}: {v} adet")
->>>>>>> ca300630efc322be52ec4652b8ea01f7244fe926
